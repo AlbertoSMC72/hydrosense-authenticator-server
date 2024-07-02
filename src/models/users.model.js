7f67014fa8836e24a847d43902985a95f93ba294
AlbertoSMC72 <123435927+AlbertoSMC72@users.noreply.github.com>
2024-07-01 21:49:19 -0600

    comit init

---
+++ src/models/users.model.js
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    surname: {
+        type: String,
+        required: false
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true, 
+        match: [/.+\@.+\..+/, "Please fill a valid email address"] 
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6 
+    },
+    position: {
+        type: String,
+        required: true,
+        default: 'user'
+    },
+});
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
