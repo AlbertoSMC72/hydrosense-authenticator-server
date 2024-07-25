9855858f334e7daa3fedf6b066cfc94022766ecd
AlbertoSMC72 <123435927+AlbertoSMC72@users.noreply.github.com>
2024-07-25 00:58:20 -0600

    chore: Update app.js to use HTTPS server with SSL certificate

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import index from './src/routes/index.router.js';
 import config from './src/config/config.js';
+import https from 'https';
+import fs from 'fs';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -19,12 +21,16 @@ app.use((req, res) => {
     res.status(404).send("Wrong route");
 });
 
+const options = {
+    key: fs.readFileSync('/etc/letsencrypt/live/hydrosense-autentificador.integrador.xyz/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/hydrosense-autentificador.integrador.xyz/fullchain.pem')
+};
+
 config.connect().then(() => {
     console.log("Database connected");
-    app.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`);
+    https.createServer(options, app).listen(PORT, () => {
+        console.log(`HTTPS Server running on port ${PORT}`);
     });
 }).catch((error) => {
     console.log("Error al iniciar el servidor:", error);
 });
-
